00bf3e7df17e90de113016eeb913f5511c318b78
Taras <fedoseevt@gmail.com>
2021-12-21 16:52:25 +0200

    first commit

--- src/javascript/components/fighterSelector.js
+++ src/javascript/components/fighterSelector.js
@@ -2,6 +2,7 @@ import { createElement } from '../helpers/domHelper';
 import { renderArena } from './arena';
 import versusImg from '../../../resources/versus.png';
 import { createFighterPreview } from './fighterPreview';
+import { fighterService } from '../services/fightersService';
 
 export function createFightersSelector() {
   let selectedFighters = [];
@@ -20,8 +21,17 @@ export function createFightersSelector() {
 const fighterDetailsMap = new Map();
 
 export async function getFighterInfo(fighterId) {
-  // get fighter info from fighterDetailsMap or from service and write it to fighterDetailsMap
+  const { _id } = fighterId;
+  if (!fighterDetailsMap.has(_id)) {
+    // send request here
+    const fighterInfo = fighterService.getFighterDetails(_id)
+    fighterDetailsMap.set(_id, fighterInfo);
+  }
+  return fighterDetailsMap.get(_id);
 }
+
+  // get fighter info from fighterDetailsMap or from service and write it to fighterDetailsMap
+
 
 function renderSelectedFighters(selectedFighters) {
   const fightersPreview = document.querySelector('.preview-container___root');
